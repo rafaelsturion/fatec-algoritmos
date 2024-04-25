253854a406b153c1740ebc03e8bac84177030f2e
rafael <joaorafaelsturion@gmail.com>
2024-04-25 11:24:58 -0300

    exercicios 4

---
+++ Exercicios 4/exercicioFix47.py
@@ -0,0 +1,21 @@
+# Solicitar ao usuário que insira um número
+num = int(input("Digite um número: "))
+
+# Nome e RA João Rafael
+print("João Rafael Sturion Mantoanelli, RA: 1051392411012")
+
+# Se o número for divisível por 10, exibir a mensagem correspondente
+if num % 10 == 0:
+    print("O número é divisível por 10.")
+
+# Se o número for divisível por 5, exibir a mensagem correspondente
+elif num % 5 == 0:
+    print("O número é divisível por 5.")
+
+# Se o número for divisível por 2, exibir a mensagem correspondente
+elif num % 2 == 0:
+    print("O número é divisível por 2.")
+
+# Se o número não for divisível por 10, 5 ou 2, exibir a mensagem correspondente
+else:
+    print("O número não é divisível por 10, 5 ou 2.")
---
+++ Exercicios 4/exercicioFix48.py
@@ -0,0 +1,22 @@
+# Solicitar ao usuário que insira seu nome e salário
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salário: "))
+
+# Se o salário for menor ou igual a 1500, adicionar um aumento de 20%
+if salario <= 1500:
+    salario += salario * 0.20
+
+# Se o salário for maior que 1500 e menor que 2500, adicionar um aumento de 10%
+elif 1500 < salario < 2500:
+    salario += salario * 0.10
+
+# Se o salário for maior ou igual a 2500, adicionar um aumento de 5%
+else:
+    salario += salario * 0.05
+
+# Nome e RA João Rafael
+print("João Rafael Sturion Mantoanelli, RA: 1051392411012")
+
+# Exibir o nome do usuário e o novo salário
+print("Nome: ", nome)
+print("Novo salário: R$", salario)
